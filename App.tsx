c64f804509b3e719b8d3349070f7344c7e2c338e
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-09-13 19:55:40 -0300

    feito alguns testes com o react native

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+interface ButtonProps{
+  title: string,
+}
+
+function Button(props: ButtonProps){
+  return (
+    <TouchableOpacity>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>asdasdasd!</Text>
+      <Button title='abc'></Button>
       <StatusBar style="auto" />
     </View>
   );
