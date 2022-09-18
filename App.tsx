1a654ecbe49a4b40ee1d8797bf63ce4b767b52ba
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-09-17 22:11:00 -0300

    adicionada função de enviar notificações

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 
 import {
@@ -8,9 +9,15 @@ import {
   Inter_900Black
 } from '@expo-google-fonts/inter';
 
+import { Subscription } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications';
+
 import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
+
+import './src/services/notificationConfigs';
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,6 +26,31 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responsePushNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  });
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.
+    addNotificationReceivedListener(notification => {
+      console.log(notification);
+    });
+    responsePushNotificationListener.current = Notifications.
+    addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      if (getNotificationListener.current && responsePushNotificationListener.current){
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responsePushNotificationListener.current);
+      }
+    }
+  },[]);
 
   return (
     <Background>
